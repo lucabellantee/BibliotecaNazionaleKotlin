974c0ff6e4cebab16232abe98d6c81250a79e98f
lucabellantee <s1097955@studenti.univpm.it>
2023-05-15 22:38:40 +0200

    aggiunto Database.kt , ma potrebbe cambiare nel tempo + aggiunto librerrie e implementation per firebase

---
+++ app/src/main/java/com/example/biblioteca_nazionale/database/Database.kt
@@ -0,0 +1,12 @@
+package com.example.biblioteca_nazionale.database
+
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+
+class Database {
+
+
+    val db = Firebase.firestore
+
+}
