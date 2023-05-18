c615c09de2fbe9195c01c7ed29e0ea29def4ace8
Giansimone Coccia <S1097845@studenti.univpm.it>
2023-05-18 15:38:53 +0200

    Modifiche generali per fragment e recyclerview

--- app/src/main/java/com/example/biblioteca_nazionale/activity/HomePageActivity.kt
+++ app/src/main/java/com/example/biblioteca_nazionale/activity/HomePageActivity.kt
@@ -5,19 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.example.biblioteca_nazionale.R
+import com.example.biblioteca_nazionale.databinding.HomePageBinding
+import com.example.biblioteca_nazionale.fragments.BlankFragment
 import com.example.biblioteca_nazionale.fragments.BookListFragment
 
 class HomePageActivity : AppCompatActivity() {
 
+    lateinit var binding: HomePageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_page)
+        binding = HomePageBinding.inflate(layoutInflater)
 
         val fragmentManager: FragmentManager = supportFragmentManager
         val bookListFragment = BookListFragment()
+        val blankFrag = BlankFragment()
 
         val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragmentContainer, bookListFragment)
+        fragmentTransaction.replace(R.id.fragmentContainer, blankFrag) //Quì serve la recyclerView
         fragmentTransaction.commit()
     }
 }
