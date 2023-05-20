f2d775e3e3df707a0410d5a090522dab58aa2438
Walter Di Sabatino <waldisab2001@gmail.com>
2023-05-20 10:14:18 +0200

    Modifiche viste

---
+++ app/src/main/java/com/example/biblioteca_nazionale/fragments/BookInfoFragment.kt
@@ -0,0 +1,21 @@
+package com.example.biblioteca_nazionale.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.biblioteca_nazionale.R
+import com.example.biblioteca_nazionale.adapter.BookListAdapter
+import com.example.biblioteca_nazionale.databinding.FragmentBookInfoBinding
+import com.example.biblioteca_nazionale.repository.BookRepository
+
+class BookInfoFragment: Fragment(R.layout.fragment_book_info) {
+
+    lateinit var binding: FragmentBookInfoBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentBookInfoBinding.bind(view)
+    }
+}
