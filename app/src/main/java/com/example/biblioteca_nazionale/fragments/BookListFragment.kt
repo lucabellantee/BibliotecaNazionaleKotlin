542da654d0a63830974bba5767b71d0e6d126cce
Giansimone Coccia <S1097845@studenti.univpm.it>
2023-05-24 15:39:32 +0200

    Commit di restore

--- app/src/main/java/com/example/biblioteca_nazionale/fragments/BookListFragment.kt
+++ app/src/main/java/com/example/biblioteca_nazionale/fragments/BookListFragment.kt
@@ -1,18 +1,12 @@
 package com.example.biblioteca_nazionale.fragments
 
-import BookInfoFragment
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.biblioteca_nazionale.R
-import com.example.biblioteca_nazionale.model.Book
 import com.example.biblioteca_nazionale.adapter.BookListAdapter
 import com.example.biblioteca_nazionale.databinding.FragmentBookListBinding
-import com.example.biblioteca_nazionale.repository.BookRepository
 import com.example.biblioteca_nazionale.viewmodel.BooksViewModel
 
 class BookListFragment : Fragment(R.layout.fragment_book_list){
