1c3cc97aec21136f9b31afc16f7a57350c12dfe1
Giansimone Coccia <S1097845@studenti.univpm.it>
2023-05-25 15:27:54 +0200

    Fragment per profilo ma non ancora funzionante

--- app/src/main/java/com/example/biblioteca_nazionale/activity/HomePageActivity.kt
+++ app/src/main/java/com/example/biblioteca_nazionale/activity/HomePageActivity.kt
@@ -1,12 +1,9 @@
 package com.example.biblioteca_nazionale.activity
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
-import androidx.lifecycle.Observer
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
