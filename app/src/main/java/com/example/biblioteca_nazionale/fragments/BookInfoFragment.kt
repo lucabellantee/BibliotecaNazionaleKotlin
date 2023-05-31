e63be33bd080cd03f40c4cb6b10e0fa7dbde15e9
Walter Di Sabatino <waldisab2001@gmail.com>
2023-05-31 12:32:48 +0200

    Modifiche

--- app/src/main/java/com/example/biblioteca_nazionale/fragments/BookInfoFragment.kt
+++ app/src/main/java/com/example/biblioteca_nazionale/fragments/BookInfoFragment.kt
@@ -1,9 +1,12 @@
+package com.example.biblioteca_nazionale.fragments
+
 import android.os.Bundle
 import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.biblioteca_nazionale.R
 import com.example.biblioteca_nazionale.databinding.FragmentBookInfoBinding
 import com.example.biblioteca_nazionale.model.Book
@@ -12,6 +15,8 @@ import java.io.Serializable
 class BookInfoFragment : Fragment(R.layout.fragment_book_info) {
 
     lateinit var binding: FragmentBookInfoBinding
+
+
     /*override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,7 +26,7 @@ class BookInfoFragment : Fragment(R.layout.fragment_book_info) {
     }*/
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        
         binding = FragmentBookInfoBinding.bind(view)
 
         /*val selectedBook = arguments?.getSerializable("selectedBook")
--- app/src/main/java/com/example/biblioteca_nazionale/fragments/BookListFragment.kt
+++ app/src/main/java/com/example/biblioteca_nazionale/fragments/BookListFragment.kt
@@ -1,15 +1,13 @@
 package com.example.biblioteca_nazionale.fragments
 
-import BookInfoFragment
-import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
 import androidx.appcompat.widget.SearchView
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.biblioteca_nazionale.R
 import com.example.biblioteca_nazionale.activity.LoginActivity
@@ -26,35 +24,12 @@ class BookListFragment : Fragment(R.layout.fragment_book_list){
 
     private val model: BooksViewModel = BooksViewModel()
 
-    /*override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_book_info, container, false)
-    }*/
-    /*override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding = FragmentBookListBinding.bind(view)
-
-        //val model: BookRepository = BookRepository()
-        //val model: BooksViewModel = ViewModelProvider(this).get(BooksViewModel::class.java)
-
-        /*model.searchBooks("Harry potter e il prigioniero di azkaban").observe(viewLifecycleOwner, { booksResponse ->
-            val adapter = BookListAdapter(booksResponse)
-            binding.recyclerViewBooks.adapter = adapter
-        })*/
-        val model: BooksViewModel = BooksViewModel()
-
-        binding.recyclerViewBooks.layoutManager = LinearLayoutManager(requireContext())
-        binding.recyclerViewBooks.adapter = BookListAdapter(model.searchBooks("Harry potter e il prigioniero di azkaban"))
-    }*/
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentBookListBinding.bind(view)
+
 
 
         /*firebaseAuth = FirebaseAuth.getInstance()
@@ -81,14 +56,8 @@ class BookListFragment : Fragment(R.layout.fragment_book_list){
 
                 adapter.setOnBookClickListener(object : BookListAdapter.OnBookClickListener{
                     override fun onBookClick(position: Int) {
-                        val bookInfoFrag = BookInfoFragment()
-                        /*val bundle = Bundle()
-                        bundle.putSerializable("selectedBook", book);
-                        bookInfoFrag.arguments = bundle
-                        val fragmentManager: FragmentManager = parentFragmentManager
-                        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-                        fragmentTransaction.replace(R.id.fragmentContainer, bookInfoFrag)
-                        fragmentTransaction.commit()*/
+                        val navController = requireActivity().findNavController(R.id.fragmentContainer)
+                        navController.navigate(R.id.action_bookListFragment_to_bookInfoFragment)
                     }
                 })
                 binding.recyclerViewBooks.adapter = adapter
@@ -103,40 +72,13 @@ class BookListFragment : Fragment(R.layout.fragment_book_list){
 
                 adapter.setOnBookClickListener(object : BookListAdapter.OnBookClickListener{
                     override fun onBookClick(position: Int) {
-                        /*val book:Book = model.getBooks()[position]
-                        val bookInfoFrag = BookInfoFragment()
-                        val bundle = Bundle()
-                        bundle.putSerializable("selectedBook", book);                bookInfoFrag.arguments = bundle
-                        val fragmentManager: FragmentManager = parentFragmentManager
-                        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-                        fragmentTransaction.replace(R.id.fragmentContainer, bookInfoFrag)
-                        fragmentTransaction.commit()*/
+                        findNavController().navigate(R.id.action_bookListFragment_to_bookInfoFragment)
                     }
                 })
                 binding.recyclerViewBooks.adapter = adapter
                 return true
             }
         })
-
-        val layoutManager = LinearLayoutManager(requireContext())
-        binding.recyclerViewBooks.layoutManager = layoutManager
-        val adapter = BookListAdapter(model.getLibriLiveData())
-
-        adapter.setOnBookClickListener(object : BookListAdapter.OnBookClickListener{
-            override fun onBookClick(position: Int) {
-                /*val action = BookListFragmentDirections.actionBookListFragmentToBookInfoFragment()
-                findNavController().navigate(action)*/
-                /*val book:Book = model.getBooks()[position]
-                val bookInfoFrag = BookInfoFragment()
-                val bundle = Bundle()
-                bundle.putSerializable("selectedBook", book);                bookInfoFrag.arguments = bundle
-                val fragmentManager: FragmentManager = parentFragmentManager
-                val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-                fragmentTransaction.replace(R.id.fragmentContainer, bookInfoFrag)
-                fragmentTransaction.commit()*/
-            }
-        })
-        binding.recyclerViewBooks.adapter = adapter
     }
 
 }
