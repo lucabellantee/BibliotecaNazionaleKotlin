d41b47a0a256b1eaf5311a59b6b75bcc0e75b126
Walter Di Sabatino <waldisab2001@gmail.com>
2023-06-04 17:49:38 +0200

    Modifiche navigation e bookInfoFragment

--- app/src/main/java/com/example/biblioteca_nazionale/model/Book.kt
+++ app/src/main/java/com/example/biblioteca_nazionale/model/Book.kt
@@ -1,10 +1,34 @@
 package com.example.biblioteca_nazionale.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
 data class Book(
     @SerializedName("id") val id: String,
     @SerializedName("volumeInfo") val info: InfoBook
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readParcelable(InfoBook::class.java.classLoader)!!
+    )
 
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeParcelable(info, flags)
+    }
 
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Book> {
+        override fun createFromParcel(parcel: Parcel): Book {
+            return Book(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Book?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
--- app/src/main/java/com/example/biblioteca_nazionale/model/InfoBook.kt
+++ app/src/main/java/com/example/biblioteca_nazionale/model/InfoBook.kt
@@ -1,5 +1,7 @@
 package com.example.biblioteca_nazionale.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
 data class InfoBook(
@@ -9,9 +11,65 @@ data class InfoBook(
     @SerializedName("publisher")val publisher: String,
     @SerializedName("publishedDate") val publishedDate: String,
     @SerializedName("imageLinks") val imageLinks: ImageLinks
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.createStringArrayList()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readParcelable(ImageLinks::class.java.classLoader)!!
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeStringList(authors)
+        parcel.writeString(description)
+        parcel.writeString(publisher)
+        parcel.writeString(publishedDate)
+        parcel.writeParcelable(imageLinks, flags)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<InfoBook> {
+        override fun createFromParcel(parcel: Parcel): InfoBook {
+            return InfoBook(parcel)
+        }
+
+        override fun newArray(size: Int): Array<InfoBook?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
 
 data class ImageLinks(
     @SerializedName("smallThumbnail") val smallThumbnail: String,
     @SerializedName("thumbnail") val thumbnail: String
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(smallThumbnail)
+        parcel.writeString(thumbnail)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<ImageLinks> {
+        override fun createFromParcel(parcel: Parcel): ImageLinks {
+            return ImageLinks(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ImageLinks?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
