231f59ef265cad8e032f56fd40fa524e02070053
lucabellantee <s1097955@studenti.univpm.it>
2023-06-21 12:33:24 +0200

    FUNZIONANTE. Lettura collection BOOK

--- app/src/main/java/com/example/biblioteca_nazionale/viewmodel/FirebaseViewModel.kt
+++ app/src/main/java/com/example/biblioteca_nazionale/viewmodel/FirebaseViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.biblioteca_nazionale.firebase.FirebaseDB
+import com.example.biblioteca_nazionale.model.BookFirebase
 import com.example.biblioteca_nazionale.model.UserSettings
 import com.example.biblioteca_nazionale.model.Users
 import com.google.firebase.firestore.DocumentSnapshot
@@ -38,11 +39,29 @@ class FirebaseViewModel: ViewModel() {
         //Log.d("/FirebaseViewModel",impostazioniData.toString())
         val libriPrenotatiData = impostazioniData?.get("libriPrenotati") as? HashMap<String, ArrayList<String>>
         val commentiData = impostazioniData?.get("commenti") as? HashMap<String, HashMap<String, String>>
-        Log.d("/FirebaseViewModel",commentiData.toString())
+       // Log.d("/FirebaseViewModel",commentiData.toString())
         val uid = data?.get("uid") as? String
         val email = data?.get("email") as? String
 
         return Users(uid.toString(),email.toString(), UserSettings(libriPrenotatiData , commentiData ))
+    }
+
+    fun getBookInfoResponseFromDB(idLibro: String): MutableLiveData<DocumentSnapshot>{
+        return firebase.getAllBookInfoFromId(idLibro)
+    }
+
+    fun getBookInfo(idLibro: String): BookFirebase{
+        var allData = this.getBookInfoResponseFromDB("ID_LIBRO")
+        val data = allData.value?.data
+        val allComment = data?.get("Commenti") as? HashMap<String, HashMap<String,HashMap<String,String>>>
+        Log.d("/FirebaseViewModel", allComment.toString())
+       // val libriPrenotatiData = impostazioniData?.get("libriPrenotati") as? HashMap<String, ArrayList<String>>
+        //val comment = allComment?.get("uid utente") as? HashMap<Any,Any>
+        //Log.d("/FirebaseViewModel", comment.toString())
+        val allRankingStar = data?.get("Stelle recensioni") as? HashMap<String,String>
+        Log.d("/FirebaseViewModel", allRankingStar.toString())
+
+        return BookFirebase(allComment!!, allRankingStar!!)
     }
 
 }
