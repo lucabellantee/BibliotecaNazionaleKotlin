4317134e0efdda5d73a495c2245a7706724706e2
Walter Di Sabatino <waldisab2001@gmail.com>
2023-06-22 18:52:17 +0200

    Funziona male

--- app/src/main/java/com/example/biblioteca_nazionale/fragments/BookInfoFragment.kt
+++ app/src/main/java/com/example/biblioteca_nazionale/fragments/BookInfoFragment.kt
@@ -3,7 +3,6 @@ package com.example.biblioteca_nazionale.fragments
 
 import RequestViewModel
 import android.annotation.SuppressLint
-import android.location.Address
 import android.os.Bundle
 import android.text.SpannableString
 import android.text.TextUtils
@@ -17,15 +16,8 @@ import com.example.biblioteca_nazionale.R
 import com.example.biblioteca_nazionale.databinding.FragmentBookInfoBinding
 import com.example.biblioteca_nazionale.model.Book
 import android.location.Geocoder
-import android.location.Location
-import android.util.Log
-import android.widget.Toast
-import com.example.biblioteca_nazionale.model.RequestBookCodes
-import com.example.biblioteca_nazionale.model.RequestBookName
-import com.example.biblioteca_nazionale.model.RequestCode
-import com.example.biblioteca_nazionale.model.RequestCodeLibrary
+import androidx.lifecycle.lifecycleScope
 import com.example.biblioteca_nazionale.model.RequestCodeLocation
-import com.example.biblioteca_nazionale.viewmodel.BooksViewModel
 import com.google.android.gms.location.LocationServices
 import java.util.Locale
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -34,23 +26,23 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.appbar.MaterialToolbar
-import com.google.gson.Gson
 import com.google.maps.GeoApiContext
 import com.google.maps.GeocodingApi
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
-import java.net.URLEncoder
-import java.security.KeyStore.TrustedCertificateEntry
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 
 class BookInfoFragment : Fragment(R.layout.fragment_book_info) {
 
     lateinit var binding: FragmentBookInfoBinding
     private lateinit var toolbar: MaterialToolbar
+
     //private lateinit var libraries: List<RequestCodeLocation>
     private val modelRequest: RequestViewModel = RequestViewModel()
-
 
 
     private var isExpanded = false
@@ -125,29 +117,21 @@ class BookInfoFragment : Fragment(R.layout.fragment_book_info) {
         mapView.onCreate(savedInstanceState)
 
 
-        val librariesNames: MutableList<RequestCodeLocation> = mutableListOf()
-
-        modelRequest.getLibraries().forEach {
-            librariesNames.add(it)
-        }
-
-        println(librariesNames)
-
-
         val cityName = "Teramo"
 
-        // Ottieni le coordinate geografiche corrispondenti al nome della città utilizzando il servizio di geocoding
         val geocoder = context?.let { Geocoder(it, Locale.getDefault()) }
 
         val addressList = geocoder?.getFromLocationName(cityName, 1)
 
-        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        val fusedLocationClient =
+            LocationServices.getFusedLocationProviderClient(requireContext())
 
         if (addressList != null) {
             if (addressList.isNotEmpty()) {
                 mapView.getMapAsync { googleMap ->
                     // Personalizzazione e visualizzazione della mappa
-                    googleMap.uiSettings.isZoomControlsEnabled = true // Abilita i controlli di zoom
+                    googleMap.uiSettings.isZoomControlsEnabled =
+                        true // Abilita i controlli di zoom
                     googleMap.uiSettings.isMyLocationButtonEnabled =
                         true // Abilita il pulsante "La mia posizione"
                     googleMap.uiSettings.isScrollGesturesEnabled =
@@ -169,52 +153,50 @@ class BookInfoFragment : Fragment(R.layout.fragment_book_info) {
                     val cameraUpdate = CameraUpdateFactory.newLatLngZoom(initialLatLng, 12f)
                     googleMap.moveCamera(cameraUpdate)
 
+                    val librariesNames: MutableList<String> = mutableListOf()
+
                     val geoApiContext = GeoApiContext.Builder()
-                        .apiKey("AIzaSyCtTj2ohggFHtNX2asYNXL1kj31pO8wO_Y")
+                        .apiKey("AIzaSyCtTj2ohggFHtNX2asYNXL1kj31pO8wO_Y") // Replace with your actual API key
                         .build()
 
+                    modelRequest.getLibraries().observe(viewLifecycleOwner) { libraries ->
+                        libraries?.let { libraryList ->
+                            // Utilizza una coroutine per attendere il completamento dell'operazione di riempimento di librariesNames
+                            lifecycleScope.launch(Dispatchers.Main){
 
-                    /*for (libraryName in librariesNames) {
-                        val geocodingResult = GeocodingApi.geocode(geoApiContext, libraryName).await()
+                                for (library in libraryList) {
+                                    val shelfmark = library.shelfmarks.firstOrNull()?.shelfmark
+                                    if (shelfmark != null) {
+                                        librariesNames.add(shelfmark)
+                                        println(shelfmark)
+                                    }
+                                }
 
-                        if (geocodingResult.isNotEmpty()) {
-                            val location = geocodingResult[0].geometry.location
-                            val libraryLatLng = LatLng(location.lat, location.lng)
+                                for (libraryName in librariesNames) {
+                                    if (libraryName.isNotEmpty()) {
+                                        val shelfmark = libraryName
 
-                            // Aggiungi un marker per la biblioteca sulla mappa
-                            val markerOptions = MarkerOptions()
-                                .position(libraryLatLng)
-                                .title(libraryName)
-                                .snippet("Seleziona questa biblioteca") // Descrizione opzionale
-                            googleMap.addMarker(markerOptions)
+                                        val geocodingResult = withContext(Dispatchers.IO) {
+                                            GeocodingApi.geocode(geoApiContext, shelfmark).await()
+                                        }
+
+                                        if (geocodingResult.isNotEmpty()) {
+                                            val location = geocodingResult[0].geometry.location
+                                            val libraryLatLng = LatLng(location.lat, location.lng)
+
+                                            println(libraryLatLng.latitude)
+
+                                            // Aggiungi un marker per la biblioteca sulla mappa
+                                            val markerOptions = MarkerOptions()
+                                                .position(libraryLatLng)
+                                                .title(shelfmark)
+                                                .snippet("Seleziona questa biblioteca") // Descrizione opzionale
+                                            googleMap.addMarker(markerOptions)
+                                        }
+                                    }
+                                }
+                            }
                         }
-                    }*/
-
-
-                    // Aggiungi il marker e le altre personalizzazioni come desiderato
-                    val markerOptions = MarkerOptions()
-                        .position(initialLatLng)
-                        .title(cityName)
-                        .snippet("La città che non dorme mai")
-                    googleMap.addMarker(markerOptions)
-
-
-                    googleMap.moveCamera(cameraUpdate)
-
-                    // Aggiungi altre personalizzazioni e funzionalità alla mappa secondo le tue esigenze
-
-                    // Esempio: Aggiungi un'interazione al clic su un marker
-                    googleMap.setOnMarkerClickListener { marker ->
-                        // Gestisci l'evento del clic sul marker
-                        // ...
-                        // Restituisci true per indicare che l'evento è stato gestito
-                        true
-                    }
-
-                    // Esempio: Aggiungi un'interazione al clic sulla mappa
-                    googleMap.setOnMapClickListener { latLng ->
-                        // Gestisci l'evento del clic sulla mappa
-                        // ...
                     }
                 }
             }
@@ -222,33 +204,34 @@ class BookInfoFragment : Fragment(R.layout.fragment_book_info) {
     }
 
 
-    override fun onResume() {
-        super.onResume()
-        val mapView: MapView = binding.mapView
+override fun onResume() {
+    super.onResume()
+    val mapView: MapView = binding.mapView
 
-        mapView.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        val mapView: MapView = binding.mapView
-        mapView.onPause()
-    }
-
-    private fun updateDescriptionText() {
-        val maxLines = if (isExpanded) Integer.MAX_VALUE else 5
-        binding.textViewDescription.maxLines = maxLines
-
-        var buttonText = ""
-        if (isExpanded) {
-            buttonText = "Leggi meno"
-            binding.textViewDescription.ellipsize = null
-        } else {
-            buttonText = "Leggi di più"
-            binding.textViewDescription.ellipsize = TextUtils.TruncateAt.END
-        }
-        val spannableString = SpannableString(buttonText)
-        spannableString.setSpan(UnderlineSpan(), 0, buttonText.length, 0)
-        binding.textMoreDescription.text = spannableString
-    }
+    mapView.onResume()
 }
+
+override fun onPause() {
+    super.onPause()
+    val mapView: MapView = binding.mapView
+    mapView.onPause()
+}
+
+private fun updateDescriptionText() {
+    val maxLines = if (isExpanded) Integer.MAX_VALUE else 5
+    binding.textViewDescription.maxLines = maxLines
+
+    var buttonText = ""
+    if (isExpanded) {
+        buttonText = "Leggi meno"
+        binding.textViewDescription.ellipsize = null
+    } else {
+        buttonText = "Leggi di più"
+        binding.textViewDescription.ellipsize = TextUtils.TruncateAt.END
+    }
+    val spannableString = SpannableString(buttonText)
+    spannableString.setSpan(UnderlineSpan(), 0, buttonText.length, 0)
+    binding.textMoreDescription.text = spannableString
+}
+}
+
--- app/src/main/java/com/example/biblioteca_nazionale/viewmodel/RequestViewModel.kt
+++ app/src/main/java/com/example/biblioteca_nazionale/viewmodel/RequestViewModel.kt
@@ -1,6 +1,7 @@
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.biblioteca_nazionale.model.Book
 import com.example.biblioteca_nazionale.model.BooksResponse
 import com.example.biblioteca_nazionale.model.RequestBookName
@@ -8,25 +9,32 @@ import com.example.biblioteca_nazionale.model.RequestCode
 import com.example.biblioteca_nazionale.model.RequestCodeLocation
 import com.google.android.gms.fido.fido2.api.common.ResidentKeyRequirement.UnsupportedResidentKeyRequirementException
 import com.google.gson.Gson
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 
+
 class RequestViewModel : ViewModel() {
 
-    private var libraries: List<RequestCodeLocation> = emptyList()
+    private val libraries: MutableLiveData<List<RequestCodeLocation>> = MutableLiveData()
     private lateinit var book: Book
+
+    fun getLibraries(): MutableLiveData<List<RequestCodeLocation>> {
+        return libraries
+    }
 
     fun fetchDataBook(book: Book) {
         this.book = book
         val bookName = this.book.info?.title?.replace(" ", "+") ?: ""
         val url = URL("http://opac.sbn.it/opacmobilegw/search.json?any=$bookName")
 
-        GlobalScope.launch(Dispatchers.IO) {
+        viewModelScope.launch(Dispatchers.IO) {
             try {
                 val connection = url.openConnection() as HttpURLConnection
                 if (connection.responseCode == 200) {
@@ -36,9 +44,8 @@ class RequestViewModel : ViewModel() {
                     inputStream.close()
 
                     Log.d("merda", request.briefRecords[0].titolo)
-
-                    // Passa il risultato di fetchDataCode come parametro
                     fetchDataCode(request)
+
                 } else {
                     // Gestisci la risposta non riuscita (es. responseCode diverso da 200)
                 }
@@ -58,7 +65,10 @@ class RequestViewModel : ViewModel() {
         var i = 0
         while (i < request.briefRecords.size) {
             val record = request.briefRecords[i]
-            val bool = record.titolo.contains(book.info.title ?: "", ignoreCase = true)
+            val formattedRecordTitolo = record.titolo.replace("\\s".toRegex(), "")
+            val formattedBookTitolo = book.info.title?.replace("\\s".toRegex(), "") ?: ""
+
+            val bool = formattedRecordTitolo.contains(formattedBookTitolo, ignoreCase = true)
             if (bool) {
                 bookCode = record.codiceIdentificativo.replace("\\", "")
                 url = URL("http://opac.sbn.it/opacmobilegw/full.json?bid=$bookCode")
@@ -67,36 +77,35 @@ class RequestViewModel : ViewModel() {
             i++
         }
 
-        try {
-            val connection = url.openConnection() as HttpURLConnection
-            if (connection.responseCode == 200) {
-                val inputStream = connection.inputStream
-                val inputStreamReader = InputStreamReader(inputStream, "UTF-8")
-                val requestCode = Gson().fromJson(inputStreamReader, RequestCode::class.java)
-                inputStream.close()
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val connection = url.openConnection() as HttpURLConnection
+                if (connection.responseCode == 200) {
+                    val inputStream = connection.inputStream
+                    val inputStreamReader = InputStreamReader(inputStream, "UTF-8")
+                    val requestCode = Gson().fromJson(inputStreamReader, RequestCode::class.java)
+                    inputStream.close()
 
-                val shelfmark = requestCode.localizzazioni[0].shelfmarks[0].shelfmark
-                if (shelfmark != null) {
-                    println(shelfmark)
-                    Log.d("dio", shelfmark)
+                    val shelfmark = requestCode.localizzazioni[0].shelfmarks[0].shelfmark
+                    if (shelfmark != null) {
+                        println(shelfmark)
+                        Log.d("suss", shelfmark)
+                    } else {
+                        println("Il valore dello shelfmark è null")
+                    }
+
+                    libraries.postValue(requestCode.localizzazioni)
+
                 } else {
-                    println("Il valore dello shelfmark è null")
+                    // Gestisci la risposta non riuscita (es. responseCode diverso da 200)
                 }
-
-                libraries = requestCode.localizzazioni
-                println(libraries[0].shelfmarks[0].shelfmark)
-            } else {
-                Log.d("else", "Error: " + connection.responseMessage)
+                connection.disconnect()
+            } catch (e: Exception) {
+                e.printStackTrace()
+                println(e)
+                // Gestisci l'eccezione
             }
-            connection.disconnect()
-        } catch (e: Exception) {
-            e.printStackTrace()
-            // Gestisci l'eccezione
         }
-
-    }
-
-    fun getLibraries(): List<RequestCodeLocation> {
-        return libraries
     }
 }
+
