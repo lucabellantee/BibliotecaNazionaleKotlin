9a3d15fcc03d532ec2902a169bcb22c868f3b670
Giansimone Coccia <S1097845@studenti.univpm.it>
2023-06-28 17:32:48 +0200

    Notifiche funzionanti

--- app/src/main/java/com/example/biblioteca_nazionale/adapter/NotificationAdapter.kt
+++ app/src/main/java/com/example/biblioteca_nazionale/adapter/NotificationAdapter.kt
@@ -5,15 +5,16 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
 import com.example.biblioteca_nazionale.R
 
-class NotificationAdapter(private val context: Context, private val notificationList: List<Pair<String, String>>) :
+class NotificationAdapter(private val notificationList: List<Pair<String, String>>) :
     RecyclerView.Adapter<NotificationAdapter.NotificationViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotificationViewHolder {
-        println(notificationList)
-        val inflater = LayoutInflater.from(context)
-        val itemView = inflater.inflate(R.layout.notification, parent, false)
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.notification, parent, false)
         return NotificationViewHolder(itemView)
     }
 
@@ -27,17 +28,20 @@ class NotificationAdapter(private val context: Context, private val notification
     }
 
     inner class NotificationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val titleTextView: TextView = itemView.findViewById(R.id.bookTitleItem)
-        private val textTextView: TextView = itemView.findViewById(R.id.bookDate)
+        private val titleTextView: TextView = itemView.findViewById(R.id.notificationTitle)
+        private val textTextView: TextView = itemView.findViewById(R.id.notificationText)
         private val logoImageView: ImageView = itemView.findViewById(R.id.notificationsImageItem)
 
         fun bind(notification: Pair<String, String>) {
             val title = notification.first
             val text = notification.second
-
             titleTextView.text = title
             textTextView.text = text
-            logoImageView.setImageResource(R.drawable.logo_welcome) // Imposta l'immagine del logo
+            Glide.with(itemView)
+                .load(R.drawable.logo_welcome)
+                .apply(RequestOptions().centerCrop())
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(logoImageView)
         }
     }
 }
--- app/src/main/java/com/example/biblioteca_nazionale/fragments/NotificationsFragment.kt
+++ app/src/main/java/com/example/biblioteca_nazionale/fragments/NotificationsFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.biblioteca_nazionale.R
 import com.example.biblioteca_nazionale.databinding.FragmentNotificationsBinding
 import com.example.biblioteca_nazionale.utils.NotificationReceiver
@@ -34,45 +35,29 @@ class NotificationsFragment : Fragment(R.layout.fragment_notifications) {
         intent.putExtra("title", "Titolo della notifica")
         intent.putExtra("text", "Testo della notifica")
 
-        binding.button234.setOnClickListener {
+        /*binding.button234.setOnClickListener {
             context.sendBroadcast(intent)
-        }
+        }*/
 
-        Log.d("Nervo", "Nervo")
-
-        // Recupera le informazioni delle notifiche dalle SharedPreferences
         val sharedPreferences = requireContext().getSharedPreferences("notification_data", Context.MODE_PRIVATE)
-        println(sharedPreferences)
-        // Recupera tutte le chiavi e i valori presenti nelle SharedPreferences
         val allEntries = sharedPreferences.all
 
-        // Stampa le chiavi e i valori delle SharedPreferences
-        for ((key, value) in allEntries) {
-            println(key +" "+value)
-        }
-        //val notificationIds = sharedPreferences.getStringSet("notification_ids", emptySet())
         val notificationIds = allEntries.keys.filter { it.startsWith("title_") }.map { it.removePrefix("title_").toInt() }.toSet()
-        println(notificationIds)
 
-        // Recupera le informazioni delle notifiche corrispondenti agli id
         val notificationList = mutableListOf<Pair<String, String>>()
         if (notificationIds != null) {
-            println(notificationIds)
             for (notificationId in notificationIds) {
-                println(notificationId)
                 val title = sharedPreferences.getString("title_$notificationId", "")
                 val text = sharedPreferences.getString("text_$notificationId", "")
-                println(title+" "+text)
                 val notificationInfo = Pair(title, text)
-                println(notificationInfo)
                 notificationList.add(notificationInfo as Pair<String, String>)
                 println(notificationList)
             }
         }
+        val layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerViewNotifications.layoutManager = layoutManager
 
-        // Imposta l'adapter per la RecyclerView
-        println(notificationList)
-        val adapter = NotificationAdapter(requireContext(), notificationList)
+        val adapter = NotificationAdapter(notificationList)
         binding.recyclerViewNotifications.adapter = adapter
     }
 
