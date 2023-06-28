9d8e732f0afd39901b14d1ffe873839711d260ef
Walter Di Sabatino <waldisab2001@gmail.com>
2023-06-28 11:19:00 +0200

    Aggiustamento recyclerView ricerca

--- app/src/main/java/com/example/biblioteca_nazionale/fragments/BookListFragment.kt
+++ app/src/main/java/com/example/biblioteca_nazionale/fragments/BookListFragment.kt
@@ -1,24 +1,17 @@
 package com.example.biblioteca_nazionale.fragments
 
 import android.os.Bundle
-import android.os.Handler
 import android.util.Log
 import android.view.View
 import android.content.Context
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.widget.SearchView
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.biblioteca_nazionale.R
-import com.example.biblioteca_nazionale.activity.LoginActivity
 import com.example.biblioteca_nazionale.adapter.BookListAdapter
 import com.example.biblioteca_nazionale.databinding.FragmentBookListBinding
-import com.example.biblioteca_nazionale.model.Book
 import com.example.biblioteca_nazionale.viewmodel.BooksViewModel
 import com.google.firebase.auth.FirebaseAuth
 
@@ -68,7 +61,7 @@ class BookListFragment : Fragment(R.layout.fragment_book_list){
             }
 
             override fun onQueryTextChange(newText: String): Boolean {
-                //performBookSearch(newText)
+                performBookSearch(newText)
                 return true
             }
         })
@@ -81,21 +74,22 @@ class BookListFragment : Fragment(R.layout.fragment_book_list){
         model.searchBooks(query)
         val layoutManager = LinearLayoutManager(requireContext())
         binding.recyclerViewBooks.layoutManager = layoutManager
-        val adapter = BookListAdapter(model.getLibriLiveData())
+        model.getLibriLiveData().observe(viewLifecycleOwner){libriList->
+            val adapter = BookListAdapter(libriList)
 
-        adapter.setOnBookClickListener(object : BookListAdapter.OnBookClickListener{
-            override fun onBookClick(position: Int) {
-                val libri = model.getLibriLiveData()
-                val libro = libri.value?.items?.get(position)
-                if (libro != null) {
-                    val action = BookListFragmentDirections.actionBookListFragmentToBookInfoFragment(libro)
-                    findNavController().navigate(action)
-                } else {
-                    // Gestisci il caso in cui il libro è nullo
+            adapter.setOnBookClickListener(object : BookListAdapter.OnBookClickListener{
+                override fun onBookClick(position: Int) {
+                    val libri = model.getLibriLiveData()
+                    val libro = libri.value?.items?.get(position)
+                    if (libro != null) {
+                        val action = BookListFragmentDirections.actionBookListFragmentToBookInfoFragment(libro)
+                        findNavController().navigate(action)
+                    } else {
+                        // Gestisci il caso in cui il libro è nullo
+                    }
                 }
-            }
-        })
-        binding.recyclerViewBooks.adapter = adapter
+            })
+            binding.recyclerViewBooks.adapter = adapter
+        }
     }
-
 }
