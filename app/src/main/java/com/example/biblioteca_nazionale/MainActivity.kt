cac061bf58180077c9620cca3c7720ceaa308cee
Giansimone Coccia <S1097845@studenti.univpm.it>
2023-06-28 21:35:13 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/biblioteca_nazionale/MainActivity.kt
+++ app/src/main/java/com/example/biblioteca_nazionale/MainActivity.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import androidx.core.app.NotificationManagerCompat
+import com.example.biblioteca_nazionale.R
 import com.example.biblioteca_nazionale.activity.HomePageActivity
 import com.example.biblioteca_nazionale.activity.LoginActivity
 import com.example.biblioteca_nazionale.activity.RegistrationActivity
@@ -15,40 +17,49 @@ import com.example.biblioteca_nazionale.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    private val notificationPermissionRequestCode = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //binding = ActivityMainBinding.inflate(layoutInflater)
-        //setContentView(binding.root)
         setContentView(R.layout.activity_main)
 
-        val loginButt : Button = findViewById<Button>(R.id.loginButtonWelcPage)
-        val regButt : Button = findViewById<Button>(R.id.loginButtonWelcPage2)
+        val loginButton: Button = findViewById(R.id.loginButtonWelcPage)
+        val regButton: Button = findViewById(R.id.loginButtonWelcPage2)
 
-        loginButt.setOnClickListener(View.OnClickListener {
+        loginButton.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
-        })
+        }
 
-        regButt.setOnClickListener(View.OnClickListener {
+        regButton.setOnClickListener {
             val intent = Intent(this, RegistrationActivity::class.java)
             startActivity(intent)
-        })
+        }
     }
 
-    /*private lateinit var binding: ActivityMainBinding
-    lateinit var username : EditText
-    lateinit var password: EditText
-    lateinit var loginButton: Button
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.loginButton.setOnClickListener(View.OnClickListener {
-            if (binding.username.text.toString() == "user" && binding.password.text.toString() == "1234"){
-                Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
+    override fun onResume() {
+        super.onResume()
+        requestNotificationPermission()
+    }
+
+    private fun requestNotificationPermission() {
+        if (!NotificationManagerCompat.from(this).areNotificationsEnabled()) {
+            val intent = Intent(
+                "android.settings.APPLICATION_DETAILS_SETTINGS",
+                android.net.Uri.parse("package:$packageName")
+            )
+            startActivityForResult(intent, notificationPermissionRequestCode)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == notificationPermissionRequestCode) {
+            if (NotificationManagerCompat.from(this).areNotificationsEnabled()) {
+                Toast.makeText(this, "Notifications enabled", Toast.LENGTH_SHORT).show()
             } else {
-                Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Notifications are not enabled. Enable notifications to receive alerts from the app.", Toast.LENGTH_SHORT).show()
             }
-        })
-    }*/
+        }
+    }
 }
