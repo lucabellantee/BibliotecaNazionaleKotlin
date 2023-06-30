5f7d6f2ac5ffdbf9135acae84efb40fc5b9a9b51
Giansimone Coccia <S1097845@studenti.univpm.it>
2023-06-30 16:33:42 +0200

    Implementato meccanismo di non modifica per utenti Google

--- app/src/main/java/com/example/biblioteca_nazionale/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/biblioteca_nazionale/fragments/ProfileFragment.kt
@@ -12,6 +12,7 @@ import androidx.navigation.fragment.findNavController
 import com.example.biblioteca_nazionale.R
 import com.example.biblioteca_nazionale.databinding.FragmentProfileBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 
 class ProfileFragment : Fragment() {
@@ -21,6 +22,7 @@ class ProfileFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
+        binding.textViewNoteModify.visibility = View.INVISIBLE
         return binding.root
     }
 
@@ -54,57 +56,113 @@ class ProfileFragment : Fragment() {
 
     fun updateAll() {
         val user = auth.currentUser
-        val newEmail = binding.editTextTextEmailAddress.text.toString()
-        val newPassword = binding.editTextTextPassword.text.toString()
 
-        if (!binding.editTextTextEmailAddress.text.isEmpty() && !binding.editTextTextPassword.text.isEmpty()) {
-            user?.updateEmail(newEmail)
-                ?.addOnCompleteListener { emailTask ->
-                    if (emailTask.isSuccessful) {
-                        Toast.makeText(context, "Email changed succesfully!", Toast.LENGTH_SHORT).show()
+        if(user != null) {
+            val providerData = user.providerData
+            val isGoogleSignIn = providerData.any { it.providerId == GoogleAuthProvider.PROVIDER_ID }
 
-                        user.updatePassword(newPassword)
-                            .addOnCompleteListener { passwordTask ->
+            if(isGoogleSignIn){
+                binding.textViewNoteModify.visibility = View.VISIBLE
+                binding.editTextTextEmailAddress.isEnabled = false
+                binding.editTextTextPassword.isEnabled = false
+                binding.editTextTextPassword2.isEnabled = false
+                binding.updateButtonFrag.isEnabled = false
+            } else {
+
+                val newEmail = binding.editTextTextEmailAddress.text.toString()
+                val newPassword = binding.editTextTextPassword.text.toString()
+
+                if (!binding.editTextTextEmailAddress.text.isEmpty() && !binding.editTextTextPassword.text.isEmpty()) {
+                    user?.updateEmail(newEmail)
+                        ?.addOnCompleteListener { emailTask ->
+                            if (emailTask.isSuccessful) {
+                                Toast.makeText(
+                                    context,
+                                    "Email changed succesfully!",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+
+                                user.updatePassword(newPassword)
+                                    .addOnCompleteListener { passwordTask ->
+                                        if (passwordTask.isSuccessful) {
+                                            Toast.makeText(
+                                                context,
+                                                "Password changed succesfully!",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        } else {
+                                            Toast.makeText(
+                                                context,
+                                                "Password change operation not completed!",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        }
+                                    }
+                                val navController = Navigation.findNavController(binding.root)
+                                navController.navigate(R.id.action_profileInfoFragment_to_credentialUpdated)
+                            } else {
+                                Toast.makeText(
+                                    context,
+                                    "Credential not changed, problems occured",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                } else if (!binding.editTextTextEmailAddress.text.isEmpty() && binding.editTextTextPassword.text.isEmpty()) {
+                    user?.updateEmail(newEmail)
+                        ?.addOnCompleteListener { emailTask ->
+                            if (emailTask.isSuccessful) {
+                                Toast.makeText(
+                                    context,
+                                    "Email changed succesfully!",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                val action =
+                                    ProfileFragmentDirections.actionProfileInfoFragmentToCredentialUpdated()
+                                findNavController().navigate(action)
+                            } else {
+                                Toast.makeText(
+                                    context,
+                                    "Email not changed, problems occured!",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                } else if (binding.editTextTextEmailAddress.text.isEmpty() && !binding.editTextTextPassword.text.isEmpty()) {
+                    if (binding.editTextTextPassword.text.toString() == binding.editTextTextPassword2.text.toString()) {
+                        user?.updatePassword(newPassword)
+                            ?.addOnCompleteListener { passwordTask ->
                                 if (passwordTask.isSuccessful) {
-                                    Toast.makeText(context, "Password changed succesfully!", Toast.LENGTH_SHORT).show()
+                                    Toast.makeText(
+                                        context,
+                                        "Password changed succesfully!",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    val action =
+                                        ProfileFragmentDirections.actionProfileInfoFragmentToCredentialUpdated()
+                                    findNavController().navigate(action)
                                 } else {
-                                    Toast.makeText(context, "Password change operation not completed!", Toast.LENGTH_SHORT).show()
+                                    Toast.makeText(
+                                        context,
+                                        "Password not changed, problems occured!",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
                                 }
                             }
-                        val navController = Navigation.findNavController(binding.root)
-                        navController.navigate(R.id.action_profileInfoFragment_to_credentialUpdated)
                     } else {
-                        Toast.makeText(context, "Credential not changed, problems occured", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            context,
+                            "Password must have the same lenght and characters",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
+                } else if (binding.editTextTextEmailAddress.text.isEmpty() && binding.editTextTextPassword.text.isEmpty()) {
+                    Toast.makeText(context, "No new credential inserted", Toast.LENGTH_SHORT).show()
                 }
-        } else if (!binding.editTextTextEmailAddress.text.isEmpty() && binding.editTextTextPassword.text.isEmpty()) {
-            user?.updateEmail(newEmail)
-                ?.addOnCompleteListener { emailTask ->
-                    if (emailTask.isSuccessful) {
-                        Toast.makeText(context, "Email changed succesfully!", Toast.LENGTH_SHORT).show()
-                        val action = ProfileFragmentDirections.actionProfileInfoFragmentToCredentialUpdated()
-                        findNavController().navigate(action)
-                    } else {
-                        Toast.makeText(context, "Email not changed, problems occured!", Toast.LENGTH_SHORT).show()
-                    }
-                }
-        } else if (binding.editTextTextEmailAddress.text.isEmpty() && !binding.editTextTextPassword.text.isEmpty()) {
-            if (binding.editTextTextPassword.text.toString() == binding.editTextTextPassword2.text.toString()) {
-                user?.updatePassword(newPassword)
-                    ?.addOnCompleteListener { passwordTask ->
-                        if (passwordTask.isSuccessful) {
-                            Toast.makeText(context, "Password changed succesfully!", Toast.LENGTH_SHORT).show()
-                            val action = ProfileFragmentDirections.actionProfileInfoFragmentToCredentialUpdated()
-                            findNavController().navigate(action)
-                        } else {
-                            Toast.makeText(context, "Password not changed, problems occured!", Toast.LENGTH_SHORT).show()
-                        }
-                    }
-            } else {
-                Toast.makeText(context, "Password must have the same lenght and characters", Toast.LENGTH_SHORT).show()
             }
-        } else if (binding.editTextTextEmailAddress.text.isEmpty() && binding.editTextTextPassword.text.isEmpty()) {
-            Toast.makeText(context, "No new credential inserted", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            Toast.makeText(context, "User not logged", Toast.LENGTH_SHORT).show()
         }
     }
 
